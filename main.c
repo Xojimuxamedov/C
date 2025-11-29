cf7b79f22a91625eae423f343289128511890b0c
Dilshod Xojimuxamedov <dilshodxdxd@gmail.com>
2025-11-29 21:40:31 +0500

    String function

--- main.c
+++ main.c
@@ -2,81 +2,101 @@
 
 int main(int argc, char const *argv[])
 {
+  //STRING
+  // boshqa tillarga o'xshab C da stringni oddiy yol bilan yarata olmaymiz
+  // C da string yasash uchun biz array dan foydalanamiz
+  // va barcha arrayning metodlari array bilan ishlashning barcha yollari bunda ham ishlaydi
+  // string char turida boladi chunki u char larning yig'indisidan tashkil topadi
+
+  char string[] = "Hello world!";
+  // biz hardoim string yozish uchun "" qo'shtirnoqdan foyalanishimiz kerak
+
+  // print qilayotganda %s dan foydalanamiz
+
+  printf("%s\n", string);
+
+  // agar biz stringdan bitta harfini olmoqchi bolsak u char bo'ladi
+  // uni print qilish uchun biz %c dan foydalanishimiz kerak boladi
+
+  printf("%c\n", string[0]); // 0 inchi index 1 inchi harf!
+
+  // undagi bitta harfni o'zgartirish uchun biz osha harfni indexidagi harfga yangi harf tayinlaymiz
+  // o'zgartirayotganimizda yangi harfni '' birtirnoq orasida belgilashimiz kerak 
+  string[0] = 'G';
+  printf("%s\n\n", string); 
   
-  // ARRAYS
-  // arraylar bitta o'zgaruvchida bir necha variablelarni saqlash uchun ishlatiladi 
+  // string daham for loop ishlaydi
 
-  int myNumbers[] = {15, 20, 68, 74};
-  // hozir 4 ta o'zgaruvchisi bor array yasadik
+  for (int i = 0; i < sizeof(string)/sizeof(string[0]); i++)
+  {
+    printf("%c ",string[i]);
+  }
+  printf("\n\n");
+
+  // stringni yasashni yana bitta usuli uni har birini alohida array elementlari qilib ko'rsatish
+  // ohirida \0 qo'yishimiz kerak bu null degani
+  // bu string tugaganini bildiradi
+  char greetings[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
+
+  // SPECIAL CHARACTERS
+  // bizda stringni buzib qo'yadigan belgilar ham boladi 
+  // ularni tuzatish uchun \ backslash ni ishlatamiz 
   
-  // variablelarni chiqarish uchun index dan foydalanamiz
-  // index 0 dan boshlanib tartibli tarzda davom etadi 0 1 2 3 ...
+  // \' 
+  // \"
+  // \\
 
-  printf("index 0 dagi variable %d\n", myNumbers[0]);
+  printf("SPECIAL \" \' \\  CHARACTERS\n\n");
 
-  // value sini o'zgartirish uchun ham indexdan foydalanib keyin o'zgartiramiz
-  myNumbers[1] = 65; // index 1 da 20 edi endi 65 boldi 
-  printf("index 1 dagi variable %d\n", myNumbers[1]);
+  // STRING FUNCTIONS
+  // C da stringning funksyalaridan foydalanish uchun string.h kutubhonasidan foydalanamiz
+  #include <string.h>
 
-  // ARRAY yaratishning yana boshqa yo'li avval array uzunligini belgilash va keyin variablelar kiritish
+  // strlen 
+  // biz buni stringda nechta char borligini bilish uchun ishlatamiz
+  printf("%zu\n", strlen(greetings)); //12
+  printf("%zu\n", sizeof(greetings)); //13
   printf("\n");
-  int secondArray[5]; // 4 uzunlikdagi array 
 
-  secondArray[0] = 10; // 0 inchi indexda turgan variablening valuesi 10
-  secondArray[1] = 20; // 1 inchi indexda turgan variablening valuesi 20
-  secondArray[2] = 30; // 2 inchi indexda turgan variablening valuesi 30
-  secondArray[3] = 40; // 3 inchi indexda turgan variablening valuesi 40
-  secondArray[4] = 50; // 4 inchi indexda turgan variablening valuesi 50
-  for (int i = 0; i < 5; i++)
-  {
-    printf("%d soni arrayning %d inchi elementi da joylashgan\n", secondArray[i], i);
-  }
+  // etibor bergan bolsangiz strlen funksyasi bitta kamroq deb korsatayapti
+  // buning sababi u faqat string ga tegishli bolgan charlarni sanaydi \0 dan tashqari
+  // sizeof funksyasi esa stringning necha bitdan iboratligini ko'rsatadi 
   
-  // !!! ESLAB QOLAMIZ ARRAY YARATAYOTGANDA VALUELAR BITTA TURDA BO'LISHI KERAK
+  char testStr[50] = "Salom dunyo";
+  printf("%zu\n", strlen(testStr)); //11
+  printf("%zu\n", sizeof(testStr)); //50
+  printf("\n");
 
-
-  // Array Size
-  // arrayni hajmini o'lchayotganda biz * sizeof * operatoridan foydalanamiz
-
-  printf("\n%zu\n", sizeof(secondArray));
-  // bu bizga 20 tini qaytaradi 
-  // chunki bitta int turidagi malumot 4 bit hajmdagi joyni egallaydi 
-  // 4 * 5 = 20
-
-
-  // GET THE NUMBER OF ELEMENTS
-  // arraydagi o'zgaruvchilar sonini aniqlash uchun biz quyidagi formuladan foydlanamiz
-
-  int length = sizeof(secondArray) / sizeof(secondArray[0]);
-  printf("arrayimizdagi elementlar soni %d ta\n\n", length);
-
-  //sizeof(secondArray) / sizeof(secondArray[0]) bu formula har doim ishlaydi
-
-
-  // MULTIDIMENSIONAL ARRAYS
-  // biz arrayni 2D 3D yoki undan kop korinishlarda yasashimiz ham mumkin 
-  // uning uchun biz arrayning ichida yana array tasaymiz
-
-  int twoD[3][4] = {{1, 5, 7, 8}, {12, 14, 15, 19}, {7, 2 , 6 ,1}};
-  // biz hozir 2d korinishida array yasadik ekranda chiqarish uchun quyidagiday qilamiz
-
-  for (int i = 0; i < 3; i++)
-  {
-    for (int j = 0; j < 4; j++)
-    {
-      printf("%d\t", twoD[i][j]);
-    }
-    printf("\n");    
-  }
-    printf("\n");    
+  // CONCATENATE STRINGS
+  // strcat bizga ikkita stringni ulashni imkonini beradi 
+  // birinchi berilgan argumentga keyingisi qo'shilib birinchisiga tenglashadi 
+  // ulanganda uning bit uzunligi ham tenglashadi  
+  char str1[20] = "Birinchi ";
+  char str2[] = "Ikkinchi!";
   
-  // variablesini o'zgartirish ham huddi oddiy arrayniki bilan bir hil 
-  printf("avval ikkinchi arrayning uchunchi elementi: %d\n", twoD[1][2]);
-  twoD[1][2] = 100;
-  printf("hozir ikkinchi arrayning uchunchi elementi: %d\n", twoD[1][2]);
+  strcat(str1,str2);
+  printf("%s\n\n", str1);
 
-  // 3D array yasash ham huddi shunday 
-  int example[2][4][3]; // va hokazo
+
+  // COPY STRINGS
+  // strcpy bu birinchi stringni ikkinchisiga bitlarigacha nusxalaydi 
+  char str3[20] = "Copy";
+  char str4[] = "";
+  
+  strcpy(str4, str3);
+  printf("%s \n",str4);
+  printf("\n\n");
+
+
+  // COMPARE STRINGS
+  // strcmp stringlarni solishtirish uchun 
+  // bunda ikkita string bir hil bolsa 0 qaytaradi teng bolmasa 0 dan boshqa son
+  char str5[] = "Copy";
+  char str6[] = "Copy";
+  char str7[] = "C";
+
+  printf("%d\n", strcmp(str5, str6));
+  printf("%d\n", strcmp(str6, str7));
 
   return 0;
 }
